6e8dcfe9e4e8cd9cfe132312b2fc4428cec7fd3c
T0mix1608 <tomix1608.t@gmail.com>
2026-03-30 23:07:22 +0100

    Update and rename key.js to generate.js

--- api/generate.js
+++ api/generate.js
@@ -2,7 +2,7 @@ let keysDB = {};
 
 module.exports = (req, res) => {
     const hwid = req.query.hwid;
-    const verified = req.query.verified; 
+    const verified = req.query.verified;
 
     if (!hwid) return res.status(400).send("Missing HWID");
     if (verified !== "true") return res.status(403).send("You must complete Work.ink first");
@@ -11,9 +11,7 @@ module.exports = (req, res) => {
 
     const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     let key = "PARADOX-";
-    for (let i = 0; i < 12; i++) {
-        key += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
+    for (let i = 0; i < 12; i++) key += chars.charAt(Math.floor(Math.random() * chars.length));
 
     keysDB[hwid] = key;
     res.status(200).send(key);
