acd1b6655c06aa5dc394dab5eed42cf4f2b7b4e6
T0mix1608 <tomix1608.t@gmail.com>
2026-03-30 22:49:41 +0100

    Update key.js

--- api/key.js
+++ api/key.js
@@ -1,1 +1,6 @@
-Paradox-Free-Access
+export default function handler(req, res) {
+  const hwid = req.query.hwid || "unknown";
+  const key = "ParadoxHub-" + hwid;
+  res.setHeader("Content-Type", "text/plain");
+  res.status(200).send(key);
+}
