c5d17776f87c01863858a625c4255ea0d4c7cc8d
T0mix1608 <tomix1608.t@gmail.com>
2026-03-30 23:01:14 +0100

    Update key.js

--- api/key.js
+++ api/key.js
@@ -1,5 +1,13 @@
+let keysDB = {};
+
 module.exports = (req, res) => {
-    const hwid = req.query.hwid || "unknown";
-    const key = "Paradox-" + hwid;
+    const hwid = req.query.hwid;
+    if (!hwid) return res.status(400).send("No HWID provided");
+    if (keysDB[hwid]) return res.status(200).send(keysDB[hwid]);
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let key = "PARADOX-";
+    for (let i = 0; i < 12; i++) key += chars.charAt(Math.floor(Math.random() * chars.length));
+    keysDB[hwid] = key;
+
     res.status(200).send(key);
 };
