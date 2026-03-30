65ea7c5150b252129b9558b613e9ae0df008a908
T0mix1608 <tomix1608.t@gmail.com>
2026-03-30 22:53:45 +0100

    Delete api/key.js

--- api/key.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  const hwid = req.query.hwid || "unknown";
-  const key = "ParadoxHub-" + hwid;
-  res.setHeader("Content-Type", "text/plain");
-  res.status(200).send(key);
-}
